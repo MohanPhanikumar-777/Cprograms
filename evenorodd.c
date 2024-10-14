be4a323977d8fc5e6bd152b425cb789075af0342
MohanPhanikumar-777 <mohanphanikumar123@gmail.com>
2024-10-14 12:19:24 +0530

    Create evenorodd.c

---
+++ evenorodd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("Enter value of a:");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("EVEN");
+    }
+    else
+    {
+    printf("ODD");
+    }
+}
