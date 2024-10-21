46f798e7985865a56112a2378cd763fb1dedfe73
MohanPhanikumar-777 <mohanphanikumar123@gmail.com>
2024-10-21 11:40:06 +0530

    Create eligibletovote18.c

---
+++ eligibletovote18.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+
+void main()
+{
+int a;
+printf("Enter value of a:");
+scanf("%d",&a);
+
+if(a>=18)
+{
+    printf(" Eligible to vote"); 
+}
+else
+{
+    printf("Not Eligible to vote");
+}
+}
