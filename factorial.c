e3ce68dd86878b0d7c55a0b0cdb7aa187d797a9e
MohanPhanikumar-777 <mohanphanikumar123@gmail.com>
2024-12-23 11:46:29 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+void main()
+{
+  int n,i;
+  long int fact=1;
+  printf("Enter n :");
+  scanf("%d",&n);
+  for(i=1;i<=n;++i)
+  {
+     fact = fact*i;
+  }
+      printf(" The factorial is %ld",fact);
+  }
+      
