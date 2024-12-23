68e4a64e4eb393e9cda8d4cd2a3c8416afc19e43
MohanPhanikumar-777 <mohanphanikumar123@gmail.com>
2024-12-23 11:58:06 +0530

    Create 1tonnumusingfor.c

---
+++ 1tonnumusingfor.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+void main()
+{
+  int n,i;
+  printf("Enter n :");
+  scanf("%d",&n);
+  for(i=1;i<=n;++i)
+  {
+      printf("%d",i);
+  }
+}
