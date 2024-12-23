2d4bce46a880a1f9e165fd631925b5ec9b421892
MohanPhanikumar-777 <mohanphanikumar123@gmail.com>
2024-12-23 12:32:49 +0530

    Create sumofallnum.c

---
+++ sumofallnum.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+void main()
+{
+  int n,i,sum=0;
+  printf("Enter n :");
+  scanf("%d",&n);
+  for(i=0;i<=n;++i)
+  {
+      sum=sum+i;
+  }
+      printf("%d",sum);
+  }
